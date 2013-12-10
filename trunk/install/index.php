2fc2721671100ef18f77ceaca8793624e87f03d2
GuillaumeGas <gas.guillaume@free.fr>
2013-12-10 18:23:40 +0100

    Ajout du formulaire de l'étape 1

--- trunk/install/index.php
+++ trunk/install/index.php
@@ -1,9 +1,6 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Guidono
  * Date: 10/12/13
- * Time: 17:40
  *
  * Script d'installation du cms
  *
@@ -29,11 +26,47 @@ function check_cms_installed() {
 }
 
 function start_step_one() {
-    
+    echo '<h1 align="center">Etape 1</h1>';
+    echo '<form method="POST" action="index.php"/>';
+    echo '<table align="center">';
+        echo '<tr>';
+            echo '<td colspan="2"><i>Creation de la base de donnees...</i></td>';
+        echo '</tr>';
+        echo '<tr>';
+            echo '<th>Serveur :</th><td><input type="text" name="server"/></td>';
+        echo '</tr>';
+        echo '<tr>';
+            echo '<th>Login   :</th><td><input type="text" name="login"/></td>';
+        echo '</tr>';
+        echo '<tr>';
+            echo '<th>Mot de passe :</th><td><input type="password" name="pass"/></td>';
+        echo '</tr>';
+        echo '<tr>';
+            echo '<th>Base de donnees :</th><td><input type="text" name="bdd"/></td>';
+        echo '</tr>';
+        echo '<tr>';
+            echo '<th colspan="2"><input type="submit" value="Ok"/>';
+        echo '</tr>';
+    echo '</table>';
+        echo '<input type="hidden" name="step" value="step_1"/>';
+    echo '</form>';
+}
+
+function start_step_two() {
+    var_dump($_POST);
 }
 
 if(isset($_POST['step'])) {
     $step = $_POST['step'];
+
+    switch($step) {
+        case "step_1":
+            start_step_two();
+            break;
+        default:
+            echo 'Erreur<br>';
+    }
+
 } else {
     /* Si nous sommes à la première étape, on vérifie qu'une installation n'a pas déjà eu lieu à l'aide d'un fichier de config */
     $cms_installed = check_cms_installed();
