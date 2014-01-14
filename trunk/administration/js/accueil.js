1e89e4b12ebcfd90f503730f9511c7507ac7a689
sasuke37 <loser6337@gmail.com>
2014-01-14 18:17:12 +0100

    ajout new article

--- trunk/administration/js/accueil.js
+++ trunk/administration/js/accueil.js
@@ -1,5 +1,11 @@
-function showmenu() {
-	menu=document.getElementById('quickIcon')
-	if (menu.style.display == 'none') menu.style.display = 'block';
-	else menu.style.display = 'none';
+function showmenu(menu, name) {
+	if (menu.style.display == 'none'){
+		menu.style.display = 'block';
+		document.images[name].src="././Images/ouvert.png";
+	}
+	else{
+		menu.style.display = 'none';
+		document.images[name].src="././Images/fermer.png";
+	}
 }
+
