bd015a310fb72b5a5ef229feaaee9cc87cde7394
emile cadorel <ecadorel@gmail.com>
2014-01-16 14:00:58 +0100

    ajout de la verification dans les fonction d'ajout

--- trunk/frontend/fonctions.php
+++ trunk/frontend/fonctions.php
@@ -5,9 +5,10 @@ require("../mysql_connect.php");
 
 function add_NEWS($bdd, $titre, $date, $contenu){
   $bdd->beginTransaction();
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $bdd->query("insert into NEWS values(null,'".$titre."','".$date."','".$contenu."')");
+  $reponse = $bdd->query("insert into NEWS values(null,'".$titre."','".$date."','".$contenu."')");
   $bdd->commit();
+  if($reponse) return true;
+  else return false;
 }
 
 function see_news($bdd){
@@ -20,134 +21,155 @@ function see_news($bdd){
 
 function add_PHOTO($bdd, $nom, $fichier,$commentaires){
   $bdd->beginTransaction();
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $bdd->query("insert into PHOTO values(null,'".$nom."','".$fichier."','".$commentaires."')");
+  $reponse = $bdd->query("insert into PHOTO values(null,'".$nom."','".$fichier."','".$commentaires."')");
   $bdd->commit();
+
+  if($reponse) return true;
+  else return false;
 }
 
 function add_TYPE_USER($bdd , $nom, $description){
   $bdd->beginTransaction();
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $bdd->query("insert into TYPE_USER values(null,'".$nom."','".$description."')");
+  if($reponse) return true;
+  else return false;
   $bdd->commit();
 }
 
 function add_UTILISATEUR($bdd, $Pseudo, $Mail, $Mdp, $IdPhoto, $IdTypeUser){
   $bdd->beginTransaction();
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $bdd->query("insert into UTILISATEUR values(null,'".$Pseudo."','".$Mail."','".$Mdp."',".$IdPhoto.",".$IdTypeUser.")");
+  $reponse = $bdd->query("insert into UTILISATEUR values(null,'".$Pseudo."','".$Mail."','".$Mdp."',".$IdPhoto.",".$IdTypeUser.")");
   $bdd->commit();
+  if($reponse) return true;
+  else return false;
 }
 
 function add_CATEGORIE($bdd, $nom, $description){
- 
   $bdd->beginTransaction();
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $bdd->query("insert into UTILISATEUR values(null,'".$nom."','".$description."')");
+$reponse = $bdd->query("insert into UTILISATEUR values(null,'".$nom."','".$description."')");
   $bdd->commit();
+  if($reponse) return true;
+  else return false;
 }
 
 function add_INSCRIT($bdd, $nom, $prenom, $taille, $poids, $position, $idphoto, $idcategorie){
   $bdd->beginTransaction();
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $bdd->query("insert into UTILISATEUR values(null,'".$nom."','".$prenom."',".$taille.",".$poids.",'".$position."',".$idphoto.",".$idcategorie.")");
-  $bdd->commit();  
+  $bdd->commit();    
+  if($reponse) return true;
+  else return false;
+  
 }
 
 function add_TEAM_ADV($bdd, $nom, $idCategorie, $Description){
   $bdd->beginTransaction();
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $bdd->query("insert into TEAM_ADV values(null,'".$nom."',".$idCategorie.",'".$Description.")");
   $bdd->commit();  
+  if($reponse) return true;
+  else return false;
+
 }
 
 function add_MATCHS($bdd, $idCategorie, $idTeamADV, $date, $heure, $lieu, $commanetaire){
   $bdd->beginTransaction();
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $bdd->query("insert into MATCHS values(null,".$idCategorie.",".$idTeamADV.",'".$date."','".$heure."','".$lieu."','".$commentaire."')");
+  $reponse = $bdd->query("insert into MATCHS values(null,".$idCategorie.",".$idTeamADV.",'".$date."','".$heure."','".$lieu."','".$commentaire."')");
   $bdd->commit();   
+  if($reponse) return true;
+  else return false;
+
 }
 
 function add_PHOTO_MATCHS($bdd, $idMatch, $idphoto){
   $bdd->beginTransaction();
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $bdd->query("insert into PHOTO_MATCHS values(".$idMatch.",".$idphoto.")");
+  $reponse = $bdd->query("insert into PHOTO_MATCHS values(".$idMatch.",".$idphoto.")");
   $bdd->commit();   
+  if($reponse) return true;
+  else return false;
 }
 
 function add_APPARTENIR_EQUIPE($bdd, $inscrit, $idMatch){
   $bdd->beginTransaction();
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $bdd->query("insert into APPARTENIR_EQUIPE values(null,".$idCategorie.",".$idTeamADV.",'".$date."','".$heure."','".$lieu."','".$commentaire."')");
+$reponse = $bdd->query("insert into APPARTENIR_EQUIPE values(null,".$idCategorie.",".$idTeamADV.",'".$date."','".$heure."','".$lieu."','".$commentaire."')");
   $bdd->commit();     
+  if($reponse) return false;
+  else return false;
 }
 
 
 function add_POSTE($bdd, $nom, $description){
   $bdd->beginTransaction();
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $bdd->query("insert into POSTE values(null,'".$nom."','".$description."')");
+  $reponse = $bdd->query("insert into POSTE values(null,'".$nom."','".$description."')");
   $bdd->commit();     
+  if($reponse) return true;
+  else return false;
 }
 
 
 function add_TYPE_PARTICIPATION($bdd, $idappart, $heureDeb, $idposte, $heureFin){
   $bdd->beginTransaction();
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $bdd->query("insert into TYPE_PARTICIPATIOn values(null,'".$idappart."','".$heureDeb."','".$idposte."','".$heureFin."')");
+  $reponse = $bdd->query("insert into TYPE_PARTICIPATIOn values(null,'".$idappart."','".$heureDeb."','".$idposte."','".$heureFin."')");
   $bdd->commit();     
+  if($reponse) return true;
+  else return false;
 }
 
 function add_POINTS($bdd, $nom, $nbpoint, $description){
   $bdd->beginTransaction();
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $bdd->query("insert into TYPE_PARTICIPATIOn values(null,'".$nom."','".$nbpoint."','".$description."')");
+  $reponse = $bdd->query("insert into TYPE_PARTICIPATIOn values(null,'".$nom."','".$nbpoint."','".$description."')");
   $bdd->commit();     
+  if($reponse) return true;
+  else return false;
 }
 
 function add_JOUEUR_ADV($bdd, $idTeamadv, $nom, $prenom, $idposte){
   $bdd->beginTransaction();
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $bdd->query("insert into JOUEUR_ADV values(null,".$idTeamADV.",'".$nom."','".$prenom."',".$poste.")");
+ $reponse = $bdd->query("insert into JOUEUR_ADV values(null,".$idTeamADV.",'".$nom."','".$prenom."',".$poste.")");
   $bdd->commit();     
+  if($reponse) return true;
+  else return false;
 }
 
 
 function add_EVENT($bdd, $idmatch, $idinscrit, $idpoints, $nom,$moment, $commentaires){
   $bdd->beginTransaction();
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $bdd->query("insert into EVENT values(null,".$idMatch.",".$idinscrit.",".$idPoints.",'".$nom."','".$moment."','".$commentaires."')");
+  $reponse = $bdd->query("insert into EVENT values(null,".$idMatch.",".$idinscrit.",".$idPoints.",'".$nom."','".$moment."','".$commentaires."')");
   $bdd->commit();     
+  if($reponse) return true; 
+  else return false;
 }
 
 function add_EVENT_ADV($bdd, $idmatch, $idjoueuradv, $idpoints, $nom, $moment, $commentaires){
   $bdd->beginTransaction();
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $bdd->query("insert into EVENT_ADV values(null,".$idMatch.",".$idjoueuradv.",".$idPoints.",'".$nom."','".$moment."','".$commentaires."')");
+ $reponse = $bdd->query("insert into EVENT_ADV values(null,".$idMatch.",".$idjoueuradv.",".$idPoints.",'".$nom."','".$moment."','".$commentaires."')");
   $bdd->commit();     
+  if($reponse) return true; 
+  else return false;
 }
 
 
 function add_TOURNOI($bdd, $nom, $description, $datedeb, $datefin){
   $bdd->beginTransaction();
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $bdd->query("insert into TOURNOI values(null,'".$nom."','".$description."','".$datedeb."','".$datefin."')");
+  $reponse = $bdd->query("insert into TOURNOI values(null,'".$nom."','".$description."','".$datedeb."','".$datefin."')");
   $bdd->commit();     
+  if($reponse) return true;
+  else return false;
 }
 
 function add_APPARTENIR_TOURNOI($bdd, $idTournoi, $idMatch){
   $bdd->beginTransaction();
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $bdd->query("insert into APPARTENIR_TOURNOI values(null,".$idTournoi.",".$idMatch.")");
+ $reponse = $bdd->query("insert into APPARTENIR_TOURNOI values(null,".$idTournoi.",".$idMatch.")");
   $bdd->commit();     
+  if($reponse) return true;
+  else return false;
 }
 
 
 function add_SITE($bdd, $nom, $URL){
   $bdd->beginTransaction();
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $bdd->query("insert into SITE values(null,'".$nom."','".$url."')");
+ $reponse = $bdd->query("insert into SITE values(null,'".$nom."','".$url."')");
   $bdd->commit();       
+  if($reponse) return true;
+  else return false;
 }
 
 
