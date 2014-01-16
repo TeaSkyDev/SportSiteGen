a9de1157d58f053106cac4ed217b510e2fee1926
emile cadorel <ecadorel@gmail.com>
2014-01-16 16:48:03 +0100

    possibilité d'avoir plusieur news commencer

--- trunk/frontend/php/news.php
+++ trunk/frontend/php/news.php
@@ -2,26 +2,38 @@
 require ("fonctions.php");
 require("../tpl/libs/Smarty.class.php");
 require("../mysql_connect.php");
-$reponse = $bdd->query("select * from NEWS");
+
+$page = 0;
+
+if(isset($_GET['page_news'])){
+  $page = $_GET['page_news'];
+}
+
+$nb_page = $bdd->query("select count(*) from NEWS")->fetch()['count(*)']/10;
+
+$reponse = $bdd->query("select * from NEWS order by id DESC");
 $smarty = new Smarty();
 $news = array();
-$post = array();
+
 $i = 0;
+
+for(; $i < 10*($page-1) ; $i++){
+  $data = $reponse->fetch();
+}
+ 
+$i = 0;
+
 while($data = $reponse->fetch()){
   $news[$i]['id'] = $data['id'];
   $news[$i]['titre'] = $data['titre'];
   $news[$i]['date'] = $data['date'];
   $news[$i]['contenu'] = $data['contenu'];
-  $news[$i]['img'] = get_PHOTO_byId($bdd, $data['IdPhoto'])['Fichier'];
-  if($i < 6){
-    $post[$i]['titre'] = $data['titre'];
-    $post[$i]['date'] = $data['date'];
-  }
+  $news[$i]['img'] = get_PHOTO_byId($bdd, $data['IdPhoto'])['Fichier']; 
   $i++;
 }
 
 
-$smarty->assign("Post", $post);
 $smarty->assign("News", $news);
 $smarty->display("html/news.html");
+
 ?>
