496c19a4f692fed3fb96ec1c4758b180f49e8b02
GuillaumeGas <guillaume@gas28.net>
2014-01-16 12:45:30 +0100

    Ajout fonction get_nb_photos et get_nb_type_user

--- trunk/frontend/fonctions.php
+++ trunk/frontend/fonctions.php
@@ -150,6 +150,24 @@ function add_SITE($bdd, $nom, $URL){
   $bdd->commit();       
 }
 
+function get_nb_photos($bdd) {
+  $nb = $bdd->query("select count(*) from PHOTO");
+  if($nb) {
+    $res = $nb->fetch();
+    return $res[0];
+  } else {
+    return -1;
+  }
+}
 
+function get_nb_type_user($bdd) {
+  $nb = $bdd->query("select count(*) from TYPE_USER");
+  if($nb) {
+    $res = $nb->fetch();
+    return $res[0];
+  } else {
+    return -1;
+  }
+}
 
 ?>
