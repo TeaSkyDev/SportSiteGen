f2db94b69e7e62cd647e57243fc65a4b61715963
GuillaumeGas <gas.guillaume@free.fr>
2014-01-24 21:31:39 +0100

    Sécurité : cas des deux mots de passes différents

--- trunk/frontend/php/inscription.php
+++ trunk/frontend/php/inscription.php
@@ -1,15 +1,19 @@
 <?php
 if(isset($_POST['pseudo']) && isset($_POST['password']) && isset($_POST['password_verif']) && isset($_POST['email'])) {
-	if(!add_UTILISATEUR($bdd, $_POST['pseudo'], $_POST['email'], md5($_POST['password']), 1, 1)) {
-		echo "erreur<br>";
-	} else {
-		$_SESSION['connected']        = true;
-		$_SESSION['user']['Pseudo']   = $_POST['pseudo'];
-		$_SESSION['user']['password'] = $_POST['password'];
-		$_SESSION['user']['Mail']     = $_POST['email'];
-		$_SESSION['user']['IdPhoto']  = 1;
-		$_SESSION['user']['IdTypeUser'] = 1;
-		header("Location: index.php");
-	}
+    if($_POST['password'] == $_POST['password_verif']) {
+        if(!add_UTILISATEUR($bdd, $_POST['pseudo'], $_POST['email'], md5($_POST['password']), 1, 1)) {
+            header("Location: index.php?page=erreur&msg=Erreur lors de l'insertion dans la BDD.");
+        } else {
+            $_SESSION['connected']        = true;
+            $_SESSION['user']['Pseudo']   = $_POST['pseudo'];
+            $_SESSION['user']['password'] = $_POST['password'];
+            $_SESSION['user']['Mail']     = $_POST['email'];
+            $_SESSION['user']['IdPhoto']  = 1;
+            $_SESSION['user']['IdTypeUser'] = 1;
+            header("Location: index.php");
+        }
+    } else {
+        header("Location: index.php?page=erreur&msg=Les mots de passe ne sont pas identiques !");
+    }
 }
 ?>
