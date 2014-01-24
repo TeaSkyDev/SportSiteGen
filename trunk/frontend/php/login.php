e28b79c5689dfcb9b9f97f051479faeb588a8e8d
GuillaumeGas <gas.guillaume@free.fr>
2014-01-24 21:47:09 +0100

    Ajout sécurité coté serveur

--- trunk/frontend/php/login.php
+++ trunk/frontend/php/login.php
@@ -3,10 +3,13 @@ require_once("../mysql_connect.php");
 /* On vérifie que des données ont été envoyées */
 if(isset($_POST['pseudo']) && isset($_POST['password'])) {
 	$pseudo = $_POST['pseudo'];
-	$pass  = htmlentities($_POST['password']);
+	$pass   = $_POST['password'];
 
 	/* On cherche un utilisateur avec le pseudo demandé */
-	$requete  = $bdd->query("select * from UTILISATEUR where Pseudo = '".$pseudo."'");
+	$requete  = $bdd->prepare("select * from UTILISATEUR where Pseudo = :pseudo");
+    $requete->bindParam(':pseudo', $pseudo);
+    $requete->execute();
+
 	if($requete) {
 		$res_pass = $requete->fetch();
 	} else {
@@ -17,7 +20,6 @@ if(isset($_POST['pseudo']) && isset($_POST['password'])) {
 	if(md5($pass) == $res_pass['Mdp'] && $res_pass['Mdp'] != null) {
 		$_SESSION['connected'] = true;
 		$_SESSION['user'] = $res_pass; //on garde en mémoire le résultat de la requete, cad les info du user
-		echo 'Vous etes maintenant connecte.<br>';
 		header("Location: index.php");
 	} else {
 		echo 'Pseudo &&/|| mot de passe incorrecte.<br><a href="index.php">Nouvelle tentative</a>';
