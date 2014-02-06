3708664c5ffbcf29d450c95ffdc04c24f84bc679
emile cadorel <ecadorel@gmail.com>
2014-02-06 15:08:40 +0100

    correction erreur de majuscule Id -> id

--- trunk/frontend/templates_c/0ef762db56418bada4bb7877f9574cbd9f4de60c.file.header.html.php
+++ trunk/frontend/templates_c/0ef762db56418bada4bb7877f9574cbd9f4de60c.file.header.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.16, created on 2014-02-05 12:44:53
+<?php /* Smarty version Smarty-3.1.16, created on 2014-02-06 15:08:09
          compiled from "html/header.html" */ ?>
 <?php /*%%SmartyHeaderCode:181489864052d911eb8d41e3-69223264%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     '0ef762db56418bada4bb7877f9574cbd9f4de60c' => 
     array (
       0 => 'html/header.html',
-      1 => 1391599817,
+      1 => 1391695682,
       2 => 'file',
     ),
   ),
@@ -27,7 +27,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   ),
   'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_52d911eba23526_99375717')) {function content_52d911eba23526_99375717($_smarty_tpl) {?><?php if (!is_callable('smarty_modifier_date_format')) include '/home/gas/www/SportSiteGen/trunk/tpl/libs/plugins/modifier.date_format.php';
+<?php if ($_valid && !is_callable('content_52d911eba23526_99375717')) {function content_52d911eba23526_99375717($_smarty_tpl) {?><?php if (!is_callable('smarty_modifier_date_format')) include '/home/cadorel/www/SportSiteGen/trunk/tpl/libs/plugins/modifier.date_format.php';
 ?><html>
   <head>
     <meta charset="UTF-8">
@@ -95,7 +95,7 @@ foreach ($_from as $_smarty_tpl->tpl_vars['post']->key => $_smarty_tpl->tpl_vars
 $_smarty_tpl->tpl_vars['post']->_loop = true;
 ?>
 	      <li>
-		<a href="index.php?page=news&id_news=<?php echo $_smarty_tpl->tpl_vars['post']->value['id'];?>
+		<a href="index.php?page=news&id_news=<?php echo $_smarty_tpl->tpl_vars['post']->value['Id'];?>
 &details=true"><?php echo $_smarty_tpl->tpl_vars['post']->value['titre'];?>
 </a> <span>Posted on <?php echo smarty_modifier_date_format($_smarty_tpl->tpl_vars['post']->value['date'],"%d-%m-%Y");?>
 </span>
