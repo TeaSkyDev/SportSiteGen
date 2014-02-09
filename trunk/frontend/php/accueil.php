d68710bd7b1bf564fb098e72c8b945c5f1b757c1
GuillaumeGas <gas.guillaume@free.fr>
2014-02-09 16:11:39 +0100

    Version de base de l'accueil fonctionelle

--- trunk/frontend/php/accueil.php
+++ trunk/frontend/php/accueil.php
@@ -1,21 +1,37 @@
 <?php
 //$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$reponse = $bdd->query("select * from NEWS order by id DESC");
+$reponse = $bdd->query("select * from NEWS order by Id DESC");
 $news    = array();
 
 $i = 0;
 while($data = $reponse->fetch()){
 	if($i < 10){
-	  $news[$i]['id'] = $data['Id'];
-	  $news[$i]['titre'] = $data['titre'];
-	  $news[$i]['date'] = $data['date'];
+	  $news[$i]['id']      = $data['Id'];
+	  $news[$i]['titre']   = $data['titre'];
+	  $news[$i]['date']    = $data['date'];
 	  $news[$i]['contenu'] = $data['contenu'];
-	  $news[$i]['img'] = get_PHOTO_byId($bdd, $data['IdPhoto'])['Fichier']; 
+	  $news[$i]['img']     = get_PHOTO_byId($bdd, $data['IdPhoto'])['Fichier'];
 	  $i++;
 	}
 }
 
+$reponse = $bdd->query("select * from EVENEMENT order by Id DESC");
+$events  = array();
+
+$i = 0;
+while($data = $reponse->fetch()){
+    if($i < 10){
+        $events[$i]['id']      = $data['Id'];
+        $events[$i]['titre']   = $data['titre'];
+        $events[$i]['date']    = $data['date'];
+        $events[$i]['contenu'] = $data['contenu'];
+        $events[$i]['lieu']    = $data['location'];
+        $i++;
+    }
+}
+
 $smarty->assign("News", $news);
+$smarty->assign("Events", $events);
 $smarty->display("html/accueil.html");
 ?>
 
