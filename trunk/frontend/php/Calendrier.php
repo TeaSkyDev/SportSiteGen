15b65bbaaf06bb3a88cda7342c341b869954849b
emile cadorel <ecadorel@gmail.com>
2014-02-13 13:31:55 +0100

    ajout de la recherche par nom dans le calendrier

--- trunk/frontend/php/Calendrier.php
+++ trunk/frontend/php/Calendrier.php
@@ -64,8 +64,68 @@ class Calendrier {
     }
 
 
+    public function search_byId($id) {
+	$query = $this->_bdd->prepare("select * from EVENEMENT where Id = :id");
+	$query->bindParam(":id", $id);
+	$query->execute();
+	if ($query->rowCount() != 0) {
+	    return $query->fetch();
+	} else {
+	    return false;
+	}
+    }
+
+
+    public function search_byName($name) {
+	$name = $name."%";
+	$query = $this->_bdd->prepare("select * from NEWS where titre like :name order bu Id DESC");
+	$query->bindParam(":name", $name);
+	$query->execute();
+	$i = 0;
+	$data = array();
+	while ($rep = $query->fetch()) {
+	    $data[$i] = $rep;
+	    $i++;
+	}
+	return $data;
+    }
+
+
+    public function search_byDate($date) {
+	$query = $this->_bdd->prepare("select * from EVENEMENT where date = :date");
+	$query->bindParam(":date", $date);
+	$query->execute();
+	if($query->rowCount() != 0) {
+	    $return;
+	    $i = 0;
+	    while($data = $query->fetch()) {
+		$return[$i] = $data;
+		$i++;
+	    }
+	    return $return;
+	} else {
+	    return false;
+	}
+    }
+
+    public function get_content_FromTo($from, $to) {
+	$data = array();
+	$i = 0;
+	while ($from + $i <= $to && isset($this->_data[$from + $i])) {
+	    $data[$i] = $this->_data[$i + $from];
+	    $i++;
+	}
+	return $data;
+    }
+
+    public function get_nb_page($nb) {
+	return $this->_nb / $nb;
+    }
 
 }
+
+
+
 
 
 
