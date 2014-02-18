66ca86e66fc1afbfc0a1ad36fdacf1af5bd3b6a7
emile cadorel <ecadorel@gmail.com>
2014-02-18 09:14:56 +0100

    possibilite d'ajout de commentaire sur les news

--- trunk/frontend/php/Content.class.php
+++ trunk/frontend/php/Content.class.php
@@ -22,14 +22,21 @@ class Content {
 
     public function get_html($page, $param = null) {
         if($page == "news") {
-
+	    
             $news_obj = new News($this->_bdd, $param);
             $news     = $news_obj->get_content();
 	    $com      = $news_obj->get_content_com();
+	    $simple = array();
+	    $simple['one'] = isset($param['v1']);
+	    if( $simple['one'] ) {
+		$simple['connected'] = isset($_SESSION['connected']);
+		$simple['Id'] = $news[0]['Id'];
+	    }
             $this->_smarty->assign("News", $news);
 	    $this->_smarty->assign("Com", $com);
+	    $this->_smarty->assign("NSimple", $simple);
             return $this->_smarty->fetch("templates/".$this->_template."/html/news.html");
-
+	    
         } else if($page == "calendrier") {
 
             $events_obj = new Calendrier($this->_bdd);
