b2f7315cec9cfaeaaa972a4c883ee1ea6b8bae67
GuillaumeGas <gas.guillaume@free.fr>
2014-02-18 08:22:12 +0100

    ajout fonctionnalité suppression d'article

--- trunk/backend/php/suppr_article.php
+++ trunk/backend/php/suppr_article.php
@@ -1,8 +1,5 @@
 <?php
 
-echo var_dump($_GET);
-echo var_dump($_POST);
-
 if(isset($_GET['id']) && isset($_GET['page'])) {
         if(Delete_byID($bdd, "NEWS", $_GET['id'])) {
             header("Location: index.php?page=article");
