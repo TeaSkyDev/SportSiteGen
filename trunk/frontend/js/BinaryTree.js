c594e5dd979e5f6d9b62a531cfb68b30ac99e56d
emile <emile@emile-HP-Pavilion-dv7-Notebook-PC.(none)>
2014-02-22 16:58:23 +0100

    ajout de la securite sur les arbre avec nombre log2(neoud) pas entier

--- trunk/frontend/js/BinaryTree.js
+++ trunk/frontend/js/BinaryTree.js
@@ -12,19 +12,22 @@ function BinaryTree (nbNode) {
 
 
     this.display = function() {
-	var tmp = this._nbNode;
-
-	for (var j = 0 ; j < Math.log(this._nbNode)/Math.LN2 + 1 ; j++) {
-	    cur_height = (this._canva_height - tmp * this._height)/2;
-	    alert(cur_height);
-	    for (var i = 0 ; i < tmp  ; i++) {
-		this.displayNode(i, (j * 110), i*this._height + (this._height/2) - this._size/2, 100, this._size);
+	if ( Math.log(this._nbNode)/Math.LN2*10%10 == 0 ) {
+	    var tmp = this._nbNode;
+	    
+	    for (var j = 0 ; j < Math.log(this._nbNode)/Math.LN2 + 1 ; j++) {
+		cur_height = (this._canva_height - tmp * this._height)/2;
+		for (var i = 0 ; i < tmp  ; i++) {
+		    this.displayNode(i, (j * 110), i*this._height + (this._height/2) - this._size/2, 100, this._size, "gold", "#000");
 		if ( i%2 == 0 && j != Math.log(this._nbNode)/Math.LN2 ) {
-		    this.displayNode("",(j+1)*110 - 10, i * this._height + (this._height/2) - this._size/2 , 10, this._height + this._size);
+		    this.displayNode("",(j+1)*110 - 10, i * this._height + (this._height/2) - this._size/2 , 10, this._height + this._size, "gold", "white");
 		}
+		}
+		tmp = tmp/2;
+		this._height = this._height*2;
 	    }
-	    tmp = tmp/2;
-	    this._height = this._height*2;
+	} else {
+	    alert("Pas possible arbre mal forme");
 	}
     }
 
@@ -32,12 +35,12 @@ function BinaryTree (nbNode) {
     
 
 
-    this.displayNode = function(text, x, y, l, h) {
+    this.displayNode = function(text, x, y, l, h, color, textcolor) {
 
-	this.context.fillStyle = "black";
+	this.context.fillStyle = color;
 	this.context.fillRect(x, y, l, h);
 	this.context.fiont = "bold " + this._textSize + "pt Calibri";
-	this.context.fillStyle = "white";
+	this.context.fillStyle = textcolor;
 	this.context.fillText(text, x, y + h/2);
     }
 
@@ -46,5 +49,5 @@ function BinaryTree (nbNode) {
 
 
 
-var arbre = new BinaryTree(16);
+var arbre = new BinaryTree(8);
 arbre.display();
