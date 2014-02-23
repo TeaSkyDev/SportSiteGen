31b8e998e9f0819d75181f304c2d95e2ff395c56
EmileCadorel <ecadorel@gmail.com>
2014-02-23 14:09:53 +0100

    ajout des fonction insertion, suppr, recherche dans Categorie

--- trunk/frontend/php/Categorie.php
+++ trunk/frontend/php/Categorie.php
@@ -28,6 +28,57 @@ class Categorie {
 
 
 
+    public function insert($nom, $description) {
+	$query = $this->_bdd->prepare("insert into CATEGORIE values(null, :nom, :desc)");
+	$query->bindParam(":nom", $nom);
+	$query->bindParam(":desc", $description);
+	$query->execute();
+	return $query->rowCount() == 1;
+    }
+
+    public function delete_byId($id) {
+	$query = $this->_bdd->prepare("delete * from CATEGORIE where Id = :id");
+	$query->bindParam(":id", $id);
+	$query->execute();
+	return $query->rowCount() == 1;
+    }
+
+
+    public function delete_byName($name) {
+	$query = $this->_bdd->prepare("delete * from CATEGORIE where Nom = :nom");
+	$query->bindParam(":nom", $name);
+	$query->execute();
+	return $query->rowCount() == 1;
+    }
+
+
+    public function search_byName($name) {
+	$query = $this->_bdd->prepare("select * from CATEGORIE where Nom = :nom");
+	$query->bindParam(":nom", $name);
+	$query->execute();
+	if ( $query->rowCount() != 0 ) {
+	    return $query->fetch();
+	} else {
+	    return false;
+	}
+    }
+
+
+    public function search_byId($id) {
+    	$query = $this->_bdd->prepare("select * from CATEGORIE where Id = :id");
+	$query->bindParam(":id", $id);
+	$query->execute();
+	if ( $query->rowCount() != 0 ) {
+	    return $query->fetch();
+	} else {
+	    return false;
+	}
+    }
+
+
+
+
+
 
 
 }
