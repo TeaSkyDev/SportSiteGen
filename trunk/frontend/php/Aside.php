c66fe1e40777cefa95464a6e32c9506138ab349d
EmileCadorel <ecadorel@gmail.com>
2014-02-23 16:13:04 +0100

    ajout de commentaire celon la syntaxe doxygen

--- trunk/frontend/php/Aside.php
+++ trunk/frontend/php/Aside.php
@@ -1,12 +1,24 @@
 <?php
 
+
+  /*============================================================
+   Classe Permettant d'obtnenir les Info sur le cote de la page
+   =============================================================
+   */
+
+
 class Aside {
-    private $_data_news;
-    private $_data_calendrier;
-    private $_nb_news;
-    private $_nb_calendrier;
+
+    private $_data_news; /* contient les news de la base */
+    private $_data_calendrier; /* contient les evenement de la base */ 
+    private $_nb_news; /* le nombre de news dans la variable _data_news */
+    private $_nb_calendrier; /* le nombre d'evenements dans la variable _data_calendrier */
 
     
+    /**
+     brief construit l'objet
+     param bdd la base de donnees a laquelle sera rattache l'objet
+     */
     public function __construct($bdd) {
 	$news = new News($bdd);
 	$cal = new Calendrier($bdd);
@@ -15,6 +27,11 @@ class Aside {
     }
 
 
+    /**
+     brief cree un tableau des 10 dernieres news 
+     param void
+     return tableau des 10 dernieres news ( ! peut etre vide )
+     */
     public function get_content_news() {
 	$i = 0;
 	$data = array();
@@ -25,6 +42,12 @@ class Aside {
 	return $data;
     }
 
+
+    /**
+     brief cree un tableau des 10 dernier evenement 
+     param void
+     return tableau des 10 derniers evenement ( ! peut etre vide )
+     */
     public function get_content_calendrier() {
 	$i = 0; 
 	$data = array();
