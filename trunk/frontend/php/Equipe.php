fb4f1885f4d54ff544b75c969ff96bb184078b6f
EmileCadorel <ecadorel@gmail.com>
2014-02-23 17:17:52 +0100

    ajout des commentaires dans la classe Equipe

--- trunk/frontend/php/Equipe.php
+++ trunk/frontend/php/Equipe.php
@@ -1,11 +1,24 @@
 <?php
 
+
+  /*
+   ====================================================
+   Classe qui gere les equipes dans la base de donnees
+   ====================================================
+  */
+
+
 class Equipe {
 
-    private $_bdd;
-    private $_nb;
-    private $_data;
+    private $_bdd; /* la base rattache a l'objet */
+    private $_nb; /* le nombre d'equipe chargees */
+    private $_data; /* les equipes chargees */
 
+
+    /**
+     \brief construit l'objet
+     \param bdd la base de donne 
+     */
     public function __construct($bdd) {
 	$this->_bdd = $bdd;
 	$this->_nb = 0;
@@ -19,11 +32,23 @@ class Equipe {
 	} 
     }
 
+    /**
+     \brief renvoi les donne chargees
+     \param void
+     \return un tableau d'equipes
+     */
     public function get_content() {
 	return $this->_data;
     }
 
 
+    /**
+     \brief ajoute une equipe a la base de donnee
+     \param nom le nom de l'equipe
+     \param photo l'identifiant de la photo de l'equipe
+     \param categorie l'identifiant de la categorie de l'equipe
+     \param description description de l'equipe
+     */
     public function insert($nom, $photo, $categorie, $description) {
 	$query = $this->_bdd->prepare("insert into TEAM values(null, :nom, :photo, :cat, :desc)");
 	$query->bindParam(":nom", $nom);
@@ -34,7 +59,11 @@ class Equipe {
 	return $query->rowCount() == 1;
     }
 
-
+    /**
+     \brief supprime une equipe en fonction de son id
+     \param id l'identifiant de l'equipe
+     \return vrai si la suppression a fonctionner faux sinon
+     */
     public function delete_byId($id) {
 	$query = $this->_bdd->prepare("delete * from TEAM where Id = :id");
 	$query->bindParam(":id", $id);
@@ -42,7 +71,11 @@ class Equipe {
 	return $query->rowCount() == 1;
     }
 
-
+    /**
+     \brief cherche une equipe en fonction de son nom
+     \param name le nom de l'equipe ( debut du nom )
+     \return renvoi une tableau d'equipe ou false
+     */
     public function search_byName($name) {
 	$name = $name."%";
 	$query = $this->_bdd->prepare("select * from TEAM where Nom like :name");
@@ -60,6 +93,11 @@ class Equipe {
     }
 
 
+    /**
+     \brief cherche une equipe en fonction de son identifiant
+     \param id l'identifiant de l'equipe
+     \return renvoi l'equipe ou false
+     */
     public function search_byId($id) {
 	$query = $this->_bdd->prepare("select * from TEAM where Id = :id");
 	$query->bindParam(":id", $id);
@@ -72,18 +110,31 @@ class Equipe {
     }
 
 
-    
+    /**
+     \brief renvoi le equipe entre une debut et une fin
+     \param from la premiere equipe a charger
+     \param to la derniere equipe a charger
+     \return un tableau d'equipe ou false
+     */
     public function get_content_FromTo($from, $to) {
-	$data = array();
-	$i = 0;
-	while ($from + $i <= $to && isset($this->_data[$from + $i])) {
-	    $data[$i] = $this->_data[$i + $from];
-	    $i++;
+	if ( isset( $this->_data[$from] ) ) {
+	    $data = array();
+	    $i = 0;
+	    while ($from + $i <= $to && isset($this->_data[$from + $i])) {
+		$data[$i] = $this->_data[$i + $from];
+		$i++;
+	    }
+	    return $data;
+	} else {
+	    return false;
 	}
-	return $data;
     }
 
-
+    /**
+     \brief renvoi le nombre de page necessaire pour afficher des equipes
+     \param nb le nomrbre d'equipe a afficher par page
+     \return le nombre de page necessaire 
+     */
     public function get_nb_page($nb) {
 	return $this->_nb / $nb;
     } 
