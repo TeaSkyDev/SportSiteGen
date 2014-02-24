923086a0822ab013d26a8d9fdc5b47fe356ef06e
EmileCadorel <ecadorel@gmail.com>
2014-02-24 11:55:35 +0100

    ajout des commentaires de la classe Site

--- trunk/frontend/php/Site.php
+++ trunk/frontend/php/Site.php
@@ -1,12 +1,22 @@
 <?php
 
+  /*
+   ======================================================
+   Classe gerant les informations du site
+   ======================================================
+   */
+
+
 class Site {
 
-    private $_bdd;
-    private $_data;
-    private $_nb;
+    private $_bdd; /* la base de donnees */
+    private $_data; /* les info sur le site */
+    private $_nb; /* le nombre d'info */
 
-
+    /**
+     \brief construit l'objet
+     \param bdd la base de donnee
+    */
     public function __construct($bdd) {
 	$this->_bdd = $bdd;
 	$query = $bdd->prepare("select * from SITE");
@@ -17,13 +27,21 @@ class Site {
 	}
     }
 
-
+    /**
+     \brief renvoi les info sur le site
+     \param void
+     \return les infos du site
+     */
     public function get_content() {
 	return $this->_data;
     }
     
 
-
+    /**
+     \brief permet de changer l'url du site
+     \param url le nouvel url
+     \return vrai si reussi faux sinon
+     */
     public function change_URL($url) {
 	$query = $this->_bdd->prepare("update SITE set URL=:url");
 	$query->bindParam(":url", $url);
@@ -31,7 +49,12 @@ class Site {
 	return $query->rowCount() == 1;
     }
 
-
+    /**
+     \brief permet de changer l'url du site ( fonction statique )
+     \param bdd la base de donnees
+     \param url le nouvel url
+     \return vrai si reussi faux sinon
+     */
     public static function s_change_URL($bdd, $url) {
 	$query = $bdd->prepare("update SITE set URL=:url");
 	$query->bindParam(":url", $url);
@@ -39,7 +62,11 @@ class Site {
 	return $query->rowCount() == 1;
     }
 
-
+    /**
+     \brief permet de changer le nom du site
+     \param name le nouveau nom du site
+     \return vrai si reussi faux sinon
+     */
     public function change_Name($name) {
 	$query = $this->_bdd->prepare("update SITE set Nom=:nom");
 	$query->bindParam(":nom", $name);
@@ -47,6 +74,12 @@ class Site {
 	return $query->rowCount() == 1;
     }
 
+    /**
+     \brief permet de changer le nom du site ( fonction statique )
+     \param bdd la base de donnees
+     \param name le nouveau nom du site
+     \return vrai si reussi faux sinon
+     */
     public static function s_change_Name($bdd, $name) {
 	$query = $bdd->prepare("update SITE set Nom=:nom");
 	$query->bindParam(":nom", $name);
@@ -54,7 +87,12 @@ class Site {
 	return $query->rowCount() == 1;
     }
 
-
+    
+    /**
+     \brief permet de changer le template
+     \param template le nouveau template
+     \return vrai si reussi faux sinon
+     */
     public function change_Template($template) {
 	$query = $this->_bdd->prepare("update SITE set current_template=:template");
 	$query->bindParam(":template", $template);
@@ -62,6 +100,12 @@ class Site {
 	return $query->rowCount() != 0;
     }
 
+    /**
+     \brief permet de changer le template ( fonction statique )
+     \param bdd la base de donnees
+     \param template le nouveau template
+     \return vrai si reussi faux sinon
+     */
     public static function s_change_Template($bdd, $template) {
 	$query = $bdd->prepare("update SITE set current_template=:template");
 	$query->bindParam(":template", $template);
