0ff40917d06153b4bfa441c5e7117573d8c8ee50
EmileCadorel <ecadorel@gmail.com>
2014-02-24 11:35:58 +0100

    ajout des commentaires classe Poste

--- trunk/frontend/php/Poste.php
+++ trunk/frontend/php/Poste.php
@@ -1,14 +1,28 @@
 <?php
 
+
+  /*
+   =============================================================
+   Classe gere les postes des Joueurs 
+   =============================================================
+   */
+
+
 class Poste {
 
-    private $_bdd;
+    private $_bdd; /*la base de donnees */
 
+    
     public function __construct($bdd) {
 	$this->_bdd = $bdd;
     }
 
-
+    /**
+     \brief insere un nouveau poste dans la base de donnees
+     \param nom le nom du poste 
+     \param desc la description du poste
+     \return vrai si reussi faux sinon
+     */
     public function insert($nom, $desc) {
 	$query = $this->_bdd->prepare("insert into POSTE values(null, :nom, :desc)");
 	$query->bindParam(":nom", $nom);
@@ -17,7 +31,11 @@ class Poste {
 	return $query->rowCount() == 1;
     }
 
-
+    /**
+     \brief supprime un poste en fonction de son id
+     \param id l'identifiant du poste
+     \return vrai si reussi faux sinon
+     */
     public function delete_byId($id) {
 	$query = $this->_bdd->prepare("delete * from POSTE where Id = :id");
 	$query->bindParam(":id", $id);
@@ -25,7 +43,12 @@ class Poste {
 	return $query->rowCount() == 1;
     }
 
-
+    
+    /**
+     \brief cherche un poste en fonction de son id
+     \param id l'identifiant du poste
+     \return un poste ou false
+     */
     public function search_byId($id) {
 	$query = $this->_bdd->prepare("select * from POSTE where Id = :id");
 	$query->bindParam(":id", $id);
@@ -37,7 +60,11 @@ class Poste {
 	}
     }
 
-
+    /**
+     \brief cherche un poste en fonction de son nom
+     \param name le nom du poste
+     \return un poste ou false
+     */
     public function search_byName($name) {
 	$query = $this->_bdd->prepare("select * from POSTE where Nom = :name");
 	$query->bindParam(":name", $name);
