467c4bb2318350735c5aa34fb14760a913c3796c
EmileCadorel <ecadorel@gmail.com>
2014-02-24 12:26:03 +0100

    correction bug mise a jour article

--- trunk/backend/php/article.php
+++ trunk/backend/php/article.php
@@ -1,4 +1,5 @@
 <?php
+include("News.php");
 
 /* Vérification des arg de l'url : si une action est demandée on la traite */
 if(isset($_GET['action'])) {
@@ -24,6 +25,14 @@ if(isset($_GET['action'])) {
             $msg = "Erreur, il manque l'id de la news à supprimer.<br>";
             header("Location: index.php?page=err&msg=".$msg);
         }
+    } else if ($action == "edit") {
+	$new = new News($bdd);
+	if ($new->update_news($_GET['id'], $_POST['titre'], date("y-m-d H:i:s"), $_POST['editor1'], 1, "Admin")) {
+	    header("Location: index.php?page=article");
+	} else {
+	    $msg = "Erreur lors de la mise a jour d'une news.";
+            header("Location: index.php?page=err&msg=".$msg);
+	}
     }
 
 } else {
