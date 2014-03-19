e2c631f9f712ec2a6a7bc43782591ee319ab5d11
GuillaumeGas <gas.guillaume@free.fr>
2014-03-19 18:48:46 +0100

    User enregistré en tant que utilisateur classique

--- trunk/frontend/php/Inscription.php
+++ trunk/frontend/php/Inscription.php
@@ -20,7 +20,7 @@ class Inscription {
     public static function insert($bdd) {
         if(isset($_POST['pseudo']) && isset($_POST['password']) && isset($_POST['password_verif']) && isset($_POST['email'])) {
             if($_POST['password'] == $_POST['password_verif']) {
-                if(!Profil::s_insert($bdd, $_POST['pseudo'], $_POST['email'], $_POST['password'], 1, 1)) {
+                if(!Profil::s_insert($bdd, $_POST['pseudo'], $_POST['email'], $_POST['password'], 1, 2)) {
                     return false;
                 } else {
                     $data = Profil::s_search_byName($bdd, $_POST['pseudo']);
