02d1e1f902a5eeabe143ff753ee3fbf3a51d8842
GuillaumeGas <gas.guillaume@free.fr>
2014-04-01 18:48:03 +0200

    fichier connexion bdd supprimé avant installation

--- trunk/install/php/fonctions.php
+++ trunk/install/php/fonctions.php
@@ -24,6 +24,7 @@ function check_cms_installed() {
 }
 
 function cms_installed() {
+    @unlink("cms.conf");
     $fichier = fopen("cms.conf", "r+");
 
     fseek($fichier, 0);
@@ -78,16 +79,13 @@ function exec_sql_file($bdd, $file) {
         $res = true;
         $i = 0;
         foreach($test as $req) {
-            //echo var_dump($req);
             if(!empty($req) && $req != ";") {
                 $r = $bdd->query($req);
                 $i++;
-                //if($i >= 19 && $i <= 36) { //On évite d'afficher les erreurs liées au 'drop table' car ces requetes ne sont là que par sécurité
-                    if(!$r) {
-                        echo '<font color="red">[!] Erreur lors de l\'éxecution de la requête n°'.$i.': '.$req.'</font><br>';
-                        $res = false;
-                    }
-                //}
+		if(!$r) {
+		    echo '<font color="red">[!] Erreur lors de l\'éxecution de la requête n°'.$i.': '.$req.'</font><br>';
+		    $res = false;
+		}
             }
         }
 
@@ -105,6 +103,7 @@ function bdd_connexion($server, $login, $pass, $bdd) {
 function create_connexion_to_bdd_file($server, $login, $pass, $bdd) {
     $code = '<?php $bdd = new PDO(\'mysql:host='.$server.';dbname='.$bdd.'\', \''.$login.'\', \''.$pass.'\'); ?>';
 
+    @unlink("../mysql_connect.php");
     $file = fopen("../mysql_connect.php", "a+");
     fseek($file, 0);
     fwrite($file, $code);
@@ -112,7 +111,6 @@ function create_connexion_to_bdd_file($server, $login, $pass, $bdd) {
 }
 
 function get_url_frontend() {
-    //"http://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"] -> Ne fonctionne pas pour certain protocoles !
     $url = "http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
     return substr($url, 0, -17)."frontend/index.php";
 }
