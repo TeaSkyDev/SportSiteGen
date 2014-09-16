3a980ed62b82e48c476a51e9015e476ad96df6c1
Guillaume Gas <guillaume@gas28.net>
2014-09-16 21:45:44 +0200

    ajout classe avec methode static pour afficher message

---
+++ trunk/frontend/php/Message.class.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fonction statique permettant d'envoyer un message (en réalité une popup)
+ */
+
+class Message {
+    public static function msg($message, $redirection, $smarty) {
+        $smarty->assign("Message", $message);
+        $smarty->assign("Redirection", $redirection);
+        return $smarty->fetch("html/message.html");
+    }
+
+    public static function bbcode($text) {
+        $text = preg_replace('!\[quote\](.+)\[/quote\]!isU', '<div class="citationforum">$1</div>', $text);
+
+        $text = preg_replace('!\[b\](.+)\[/b\]!isU', '<strong>$1</strong>', $text);
+        $text = preg_replace('!\[i\](.+)\[/i\]!isU', '<em>$1</em>', $text);
+        $text = preg_replace('!\[u\](.+)\[/u\]!isU', '<span style="text-decoration:underline;">$1</span>', $text);
+        $text = preg_replace('!\[center\](.+)\[/center\]!isU', '<p tyle="text-align:center;margin:0px;padding:0px;">$1</p>', $text);
+        $text = preg_replace('!\[right\](.+)\[/right\]!isU', '<p style="text-align:right;margin:0px;padding:0px;">$1</p>', $text);
+        $text = preg_replace('!\[left\](.+)\[/left\]!isU', '<p style="text-align:left;margin:0px;padding:0px;">$1</p>', $text);
+
+        $text = preg_replace('!\[titre\](.+)\[/titre\]!isU', '<h3>$1</h3>',$text);
+
+        $text = preg_replace('!\[email\](.+)\[/email\]!isU', '<a href="mailto:$1">$1</a>',$text);
+
+        $text = preg_replace('!\[img\](.+)\[/img\]!isU', '<img src="$1" border="0">',$text);
+
+        $text = preg_replace('!\[url\](.+)\[/url\]!isU', '<a href="$1" target="_blank">$1</a>',$text);
+
+        return($text);
+    }
+}
+
+?>
