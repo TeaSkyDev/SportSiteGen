ce2dd0cdf0ad96c884e443c2dd2356c9a7e9da06
Guillaume Gas <guillaume@gas28.net>
2014-09-17 00:04:25 +0200

    Fichiers renommés en anglais

---
+++ trunk/frontend/php/Home.class.php
@@ -0,0 +1,21 @@
+<?php
+
+class Accueil {
+
+    private $bdd;
+    private $smarty;
+
+    public function __construct($bdd, $smarty) {
+        $this->_bdd    = $bdd;
+        $this->_smarty = $smarty;
+    }
+
+    public function get_contenu() {
+        //$this->get_news();
+        //$this->get_events();
+        return $this->_smarty->fetch(TEMPLATE."/html/home.html");
+    }
+
+}
+
+?>
